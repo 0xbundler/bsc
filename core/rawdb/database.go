75b9eb3b67e8239b5a5a06ec2e3f343c7fdf7d4e
0xbundler <124862913+0xbundler@users.noreply.github.com>
2024-01-24 11:27:10 +0800

    fix: fix some cr comments;

--- core/rawdb/database.go
+++ core/rawdb/database.go
@@ -594,12 +594,13 @@ func openKeyValueDatabase(o OpenOptions) (ethdb.Database, error) {
 // The passed o.AncientDir indicates the path of root ancient directory where
 // the chain freezer can be opened.
 const shardNum = 2
+const shardCacheRadio = 0.7
 
 func Open(o OpenOptions) (ethdb.Database, error) {
 	// reset sharding option
 	shardOpt := o
 	shardOpt.Handles = int(float64(o.Handles) / (shardNum + 1))
-	shardOpt.Cache = int(float64(o.Cache) * 0.5 / shardNum)
+	shardOpt.Cache = int(float64(o.Cache) * shardCacheRadio / shardNum)
 	if shardOpt.Cache < ethdb.MinDatabaseCache {
 		log.Warn("database cache is too low", "resize", ethdb.MinDatabaseCache, "actual", shardOpt.Cache)
 		shardOpt.Cache = ethdb.MinDatabaseCache
@@ -613,7 +614,7 @@ func Open(o OpenOptions) (ethdb.Database, error) {
 	// reset maindb option
 	mainOpt := o
 	mainOpt.Handles = int(float64(o.Handles) / (shardNum + 1))
-	mainOpt.Cache = int(float64(o.Cache) * 0.5)
+	mainOpt.Cache = int(float64(o.Cache) * (1 - shardCacheRadio))
 	if mainOpt.Cache < ethdb.MinDatabaseCache {
 		log.Warn("database cache is too low", "resize", ethdb.MinDatabaseCache, "actual", mainOpt.Cache)
 		mainOpt.Cache = ethdb.MinDatabaseCache
